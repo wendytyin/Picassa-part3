9ce6ac14722d24592632e221e013056bed26ac51
Wendy Yin <wty3@duke.edu>
2012-02-07 19:56:53 -0500

    now returns random static-y colors

--- src/expressions/RandomExpression.java
+++ src/expressions/RandomExpression.java
@@ -4,36 +4,44 @@ import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 import model.RGBColor;
 
-public class RandomExpression extends ParenExpression{
+public class RandomExpression extends ParenExpression {
 
-	private static final String myCommand="random";
-    private static final String commandMatching="random";
-    private static final Pattern COMMAND_REGEX = Pattern.compile(commandMatching);
-  private static final Random random=new Random();
-  
-	private RandomExpression(){}
+    private static final String myCommand = "random";
+    private static final String commandMatching = "random";
+    private static final Pattern COMMAND_REGEX = Pattern
+            .compile(commandMatching);
+    private static final Random random = new Random();
 
-	@Override
-	Expression getNewExpression() {
-		return new RandomExpression();
-	}
+    private RandomExpression() {
+    }
 
-	public static ExpressionFactory getFactory() {
-		return new ExpressionFactory(new RandomExpression());
-	}
-	
-	@Override
-	public RGBColor evaluate() {
-	    double r=random.nextDouble();
-	    if (random.nextBoolean()){
-	        r=-1*r;
-	    }
-		return new RGBColor(r);
-	}
-	
+    @Override
+    Expression getNewExpression() {
+        return new RandomExpression();
+    }
+
+    public static ExpressionFactory getFactory() {
+        return new ExpressionFactory(new RandomExpression());
+    }
+
+    @Override
+    public RGBColor evaluate() {
+        double red = random.nextDouble();
+        double green = random.nextDouble();
+        double blue = random.nextDouble();
+        
+        return new RGBColor(switchSignsRandomly(red), switchSignsRandomly(green), switchSignsRandomly(blue));
+    }
+
+    private double switchSignsRandomly(double number) {
+        if (random.nextBoolean()) {
+            return -1 * number;
+        }
+        return number;
+    }
+
     @Override
     String getMyCommand() {
         return myCommand;
